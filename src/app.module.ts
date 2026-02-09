d15467b8b61ea1f04d0e3aeb00c44b4c79e7df1e
IVAN LUCHYK <ivanluch13@gmail.com>
2026-02-09 16:56:29 +0200

    set

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import 'dotenv/config';
+import { LeadModule } from './lead/lead.module';
+import { DealModule } from './deal/deal.module';
+import { TaskModule } from './task/task.module';
+import { NoteModule } from './note/note.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false },
+      synchronize: true,
+    }),
+    AuthModule,
+    LeadModule,
+    DealModule,
+    TaskModule,
+    NoteModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import 'dotenv/config';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.use(cookieParser());
+  app.enableCors({
+    origin: '*',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+
+  const port = Number(process.env.PORT) || 3001;
+
+  await app.listen(port);
+  console.log('Server started on PORT:', port);
 }
 bootstrap();
